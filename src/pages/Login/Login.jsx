c2fe54e31b82411379a7b710932911922ac20e1f
Jafar Iqbal <Iqbal.jafer@ARTIC2.LOCAL>
2024-04-07 23:01:56 +0300

    next commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,83 @@
+import { Link,useLocation,useNavigate } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
-
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <h2 className="text-3xl">This is Login</h2>
-        </div>
-    );
+    const { signIn } = useContext(AuthContext);
+    const location = useLocation()
+    const navigate = useNavigate()
+    console.log("location in the login page",location)
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+    signIn(email, password)
+      .then((result) => {
+          console.log(result.user);
+
+          //   navigate after login
+          navigate(location?.state?location.state:'/')
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h2 className="text-3xl text-center">Please Login</h2>
+        <form
+          onSubmit={handleLogin}
+          className="card-body mx-auto md:w-3/4 lg:1/2"
+        >
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center">
+          Do not have an account{" "}
+          <Link className="text-blue-600 font-bold" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
